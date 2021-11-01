51364a6a68ee50b565a714766b6ed281b1ad87e8
Sergey Smolnikov <ssmol@equinor.com>
2021-11-01 13:14:01 +0100

    Added load calculation logic

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+from random import randrange
 from flask import Flask
 from prometheus_client import Counter, Summary, generate_latest
 
@@ -9,10 +10,19 @@ REQUEST_COUNT = Counter('custom_request_count', 'Request count')
 REQUEST_TIME = Summary('custom_request_processing_seconds', 'Time spent processing request')
 
 
+# Method to perform some load on CPU and different response time on requests
+def calculate_factorial():
+    fact = 1
+    n = randrange(5000, 20000)
+    for i in range(1, n):
+        fact = fact * i
+
+
 @app.route('/')
 @REQUEST_TIME.time()  # Decorate function with metric.
 def hello_world():
     REQUEST_COUNT.inc(1)
+    calculate_factorial()
     return 'Hello, Radix!'
 
 # Metrics endpoint
