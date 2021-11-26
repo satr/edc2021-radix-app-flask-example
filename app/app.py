c705c7a58b467ae3ab98af66f89a8335e1ef74c2
Sergey Smolnikov <ssmol@equinor.com>
2021-11-26 14:05:04 +0100

    Added envs

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from random import randrange
 from flask import Flask
-from prometheus_client import Counter, Summary, generate_latest
+from prometheus_client import Counter, Summary, Gauge, generate_latest
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 REQUEST_COUNT = Counter('custom_request_count', 'Request count')
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('custom_request_processing_seconds', 'Time spent processing request')
+REQUEST_MEMORY = Gauge(name='container_memory_usage_bytes', documentation='Memory used')
 
 
 # Method to perform some load on CPU and different response time on requests
@@ -23,7 +24,11 @@ def calculate_factorial():
 def hello_world():
     REQUEST_COUNT.inc(1)
     calculate_factorial()
-    return 'Hello, Radix!'
+    memory = REQUEST_MEMORY.describe()
+    val = 0
+    if len(memory) > 0 and len(memory[0].samples) > 0:
+        val = memory[0].samples[len(memory[0].samples) - 1]
+    return f'Hello, Radix! Memory: {val} bytes'
 
 # Metrics endpoint
 @app.route('/metrics')
